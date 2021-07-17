7cf8b5f3e87bd5fe1fbc6da1f5c8d4de1a765b25
Parsa <parsa1997.ya@gmail.com>
2021-07-18 00:15:22 +0430

    Some changes

--- front/src/components/Toolbar/Toolbar.jsx
+++ front/src/components/Toolbar/Toolbar.jsx
@@ -2,6 +2,7 @@
 import React, { useCallback } from 'react'
 import { fabric } from 'fabric'
 import { makeStyles } from '@material-ui/core/styles'
+import codegen from '../../codegen.js'
 // helper components
 import { IconButton } from '@material-ui/core'
 import ImageOutlinedIcon from '@material-ui/icons/ImageOutlined'
@@ -63,7 +64,7 @@ const useStyles = makeStyles({
   },
 })
 
-const Toolbar = ({ canvas, onSave, onLoad }) => {
+const Toolbar = ({ canvas }) => {
   const classes = useStyles()
 
   const addImage = useCallback(
@@ -86,7 +87,9 @@ const Toolbar = ({ canvas, onSave, onLoad }) => {
           // end fabricJS stuff
         }
       }
-      if (e.target && e.target.files) reader.readAsDataURL(e.target.files[0])
+      if (e.target && e.target.files) {
+        reader.readAsDataURL(e.target.files[0])
+      }
     },
     [canvas],
   )
@@ -119,7 +122,7 @@ const Toolbar = ({ canvas, onSave, onLoad }) => {
           className={classes.iconButton}
           onClick={e => {
             e.stopPropagation()
-            onSave()
+            codegen(canvas.toDatalessJSON().objects)
           }}
         >
           <SaveOutlinedIcon className={classes.icon} />
@@ -130,7 +133,7 @@ const Toolbar = ({ canvas, onSave, onLoad }) => {
           className={classes.iconButton}
           onClick={e => {
             e.stopPropagation()
-            onLoad()
+            // onLoad()
           }}
         >
           <OpenInBrowserOutlinedIcon className={classes.icon} />
