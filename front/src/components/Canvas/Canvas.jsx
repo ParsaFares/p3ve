b0a9e77e3998d182525c02c9aea09b51606786c8
Parsa <parsa1997.ya@gmail.com>
2021-07-18 00:54:11 +0430

    Zoom and pan

--- front/src/components/Canvas/Canvas.jsx
+++ front/src/components/Canvas/Canvas.jsx
@@ -23,6 +23,45 @@ const Canvas = ({ setCanvas, setSelectedObject }) => {
     canvas.on('mouse:down', function (options) {
       setSelectedObject(options.target ? options.target : null)
     })
+
+    canvas.on('mouse:wheel', function (opt) {
+      var delta = opt.e.deltaY;
+      var zoom = canvas.getZoom();
+      zoom *= 0.999 ** delta;
+      if (zoom > 20) zoom = 20;
+      if (zoom < 0.01) zoom = 0.01;
+      canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+      opt.e.preventDefault();
+      opt.e.stopPropagation();
+    })
+
+    canvas.on('mouse:down', function (opt) {
+      var evt = opt.e;
+      if (evt.altKey === true) {
+        this.isDragging = true;
+        this.selection = false;
+        this.lastPosX = evt.clientX;
+        this.lastPosY = evt.clientY;
+      }
+    })
+    canvas.on('mouse:move', function (opt) {
+      if (this.isDragging) {
+        var e = opt.e;
+        var vpt = this.viewportTransform;
+        vpt[4] += e.clientX - this.lastPosX;
+        vpt[5] += e.clientY - this.lastPosY;
+        this.requestRenderAll();
+        this.lastPosX = e.clientX;
+        this.lastPosY = e.clientY;
+      }
+    })
+    canvas.on('mouse:up', function (opt) {
+      // on mouse up we want to recalculate new interaction
+      // for all objects, so we call setViewportTransform
+      this.setViewportTransform(this.viewportTransform);
+      this.isDragging = false;
+      this.selection = true;
+    })
   }, [])
 
   return <canvas id="canvas" className={classes.canvas} />
